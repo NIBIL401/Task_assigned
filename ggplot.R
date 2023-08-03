121994c9b24a8a1bcd9595cb5107c43722601da0
NIBIL401 <61827330+NIBIL401@users.noreply.github.com>
2023-08-03 15:48:18 +0530

    Update ggplot.R

--- ggplot.R
+++ ggplot.R
@@ -1,9 +1,8 @@
 library(readr)
 library(ggplot2)
 library(dplyr)
-
-
 input_file="D:/Me/public-archivedwl-862/Homo_sapiens.gene_info"
+output_file="D:/Me/public-archivedwl-862/gene_count_per_chromosome.png"
 # Read gene information from the file and specify header is present
 gene_info <- read_tsv(input_file)
 
@@ -23,9 +22,12 @@ gene_count_per_chromosome <- gene_info_filtered %>%
   summarise(Number_of_Genes = n())
 
 # Create a bar plot using ggplot2
-ggplot(gene_count_per_chromosome, aes(x = chromosome, y = Number_of_Genes)) +
+plot=ggplot(gene_count_per_chromosome, aes(x = chromosome, y = Number_of_Genes)) +
   geom_bar(stat = "identity", fill = "grey35") +
-  labs(title = "Number of Genes per Chromosome in Human Genome",
+  labs(title = "Number of Genes in each Chromosome",
        x = "Chromosomes", y = "Number of Genes") +
-  theme_minimal()+theme(axis.line = element_line(size = .5, colour = "black"))+
-  theme(panel.grid = element_blank())
+  theme_minimal()+theme(axis.line = element_line(size = .5, colour = "black"),
+                        panel.background = element_rect(fill = "white"),  # Set panel background to white
+                        plot.background = element_rect(fill = "white"),   # Set plot background to white
+                        panel.grid = element_blank())
+ggsave(output_file, width = 8, height = 6, dpi = 300)
